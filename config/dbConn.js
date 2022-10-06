dbf30c4f07c4c9635eb12e6a63c0451511b27381
EngNelson <bechem200@gmail.com>
2022-10-06 08:18:56 +0100

    crud endpoints runing

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.DATEBASE_URI)
+        await mongoose.connect(process.env.DATABASE_URI)
     } catch (err) {
         console.log(err)
     }
